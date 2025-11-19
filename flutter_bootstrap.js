e25c83a27aac6e22e40c83c243af1b8c2361db70
temim <suveydt@gmail.com>
2025-11-19 15:03:20 +0300

    v 1.2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3418597304"
+    serviceWorkerVersion: "3762568557"
   }
 });
